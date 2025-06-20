562b1c159d61ec247f30444b7ea0ed19a6923d06
Sandro Carvalho <sandrinho9797@outlook.com>
2025-06-20 16:00:49 +0000

    make  more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -9,12 +9,20 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   })
   await client.connect()
+  try {
 
-  const result = await client.query(queryObject)
+    const result = await client.query(queryObject)
+    return result
+  } catch (error) {
+    console.error('Database query error:', error)
 
-  await client.end()
+  } finally {
 
-  return result
+    await client.end()
+  }
+
+
+
 }
 
 export default {
